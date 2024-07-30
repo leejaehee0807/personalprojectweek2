d994bff00c04fb716e71f73a2a7575eddadd94d3
leejaehee <leejaehee0807@naver.com>
2024-07-30 16:14:25 +0900

    Level 1 -요구사항 1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,19 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        //  첫번째 양의 정수 입력
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        // 두번째 양의 정수 입력
+        int num2 = sc.nextInt();
+        System.out.println("num1, num2="+num1+","+num2);
 
     }
 }
