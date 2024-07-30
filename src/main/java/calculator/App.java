de1ab57f2912b5f7376d187945bae37fa21c3279
leejaehee <leejaehee0807@naver.com>
2024-07-30 17:27:39 +0900

    Level 1 -요구사항 5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
@@ -7,6 +8,12 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String question = "go";
+        /* 요구사항 5 , 연산결과 10개를 저장할 수 있는 배열 선언 및 생성*/
+        int [] intArrays = new int[10];
+        /*연산 저장할 떄마다 카운트용 */
+        int count =0;
+
+        /* 요구사항 4 반복문을 이용한  무한연산, exit 입력하면 종료*/
         while (!question.equals("exit")) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             //  첫번째 양의 정수 입력
@@ -33,7 +40,12 @@ public class App {
                 }
                 result = num1 / num2;
             }
+            /* 배열에 결과 값을 저장 후 count 를 증가시켜 다음 index로 넘어가기 위함 */
+            intArrays[count]=result;
+            count+=1;
+
             System.out.println("결과: " + result);
+            System.out.println(Arrays.toString(intArrays));  // 확인차 배열 출력
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             question = sc.next();
         }
