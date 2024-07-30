ba609139b8c8f84336d8c5c8d76f0ec4b9bd31c9
leejaehee <leejaehee0807@naver.com>
2024-07-30 16:39:43 +0900

    Level 1 -요구사항 3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -14,12 +14,27 @@ public class App {
         // 두번째 양의 정수 입력
         int num2 = sc.nextInt();
 
+        int result =0;
+
         System.out.println("사칙연산 기호를 입력하세요:"); // +, - , *, /
         /* charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환 하고자 하는 문자열의 위치를 받는다*/
         char operator= sc.next().charAt(0);
 
-
-        System.out.println("operator="+operator);
-
+        /* 요구사항 3 - 입력연산에 관한 코드 ( 덧셈, 뺄쎔, 곱셈 나눗셈 ) */
+        /* if 문안에 operator의 연산이 맞는지 확인하기 위해 작은 따옴표 '' 사용*/
+        /* ''는 char 타입, ""는 string 타입 */
+        if ( operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*'){
+            result = num1 * num2;
+        } else if (operator == '/'){
+            if (num2 ==0){
+                System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+            }
+            result = num1/num2;
+        }
+        System.out.println("결과: "+result);
     }
 }
